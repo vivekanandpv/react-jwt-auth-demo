8a0417499f6a12a92d655abf0525f8169804b69e
vivek <vivek@athenaeum.in>
2021-09-29 16:17:49 +0530

    done

---
+++ src/utils/jwt-utils.js
@@ -0,0 +1,19 @@
+export function decode(token) {
+  if (typeof token !== 'string') {
+    throw new Error('Token type is not valid');
+  }
+
+  const parts = token.split('.');
+  return JSON.parse(atob(parts[1]));
+}
+
+export function isAlive(payload) {
+  if (!payload.exp) {
+    return true;
+  }
+
+  const expiry = new Date(payload.exp);
+  const now = new Date();
+
+  return now <= expiry;
+}
